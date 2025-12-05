c4e2f96459e1d897cb0510a374a19cbbe27eeb6e
Usman24 <usman5583@gmail.com>
2025-12-06 00:07:54 +0530

    Update axios.js

--- pharmalink-frontend/src/utils/axios.js
+++ pharmalink-frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with custom config
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://pharmalink-website.onrender.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
